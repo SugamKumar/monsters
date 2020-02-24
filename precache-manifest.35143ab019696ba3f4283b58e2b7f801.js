6d63e84debd6afd94686f1978b63206fb9e5e936
Sugam Kumar <sugamsws@gmail.com>
2020-02-24 20:30:18 +0530

    Updates

---
+++ precache-manifest.35143ab019696ba3f4283b58e2b7f801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a237cb1aaf06cdab69d5480c198700",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "ba3bfc5ec495aa93b1ef",
+    "url": "/monsters/static/css/main.fa2f34f1.chunk.css"
+  },
+  {
+    "revision": "4beba386462ee8d71d1c",
+    "url": "/monsters/static/js/2.a099693a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.a099693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3bfc5ec495aa93b1ef",
+    "url": "/monsters/static/js/main.eda18a77.chunk.js"
+  },
+  {
+    "revision": "255868a8cfcf2a3f9793",
+    "url": "/monsters/static/js/runtime-main.b78649e3.js"
+  }
+]);
